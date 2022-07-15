3320a7ac7b5c68e1ed6ae186c2137fcda40d27db
Paul9706 <63858745+paul96-loop@users.noreply.github.com>
2022-07-15 20:51:36 +0000

    edited submission

--- src/assignment.js
+++ src/assignment.js
@@ -13,24 +13,16 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
-function sumOfNumbers(array) {
+function sumOfNumbers(arrayOfNumbers) {
   let sum =0
-  for (let index = 0; index < array.length; index++) {
-  sum += array[index]  
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+  sum += arrayOfNumbers[index]  
   }
   
 
   return sum;
 }
-const number=[10,20,30,40,60]
-sumOfNumbers(number)
-console.log(sumOfNumbers(number))
-
-
-
-
-
- // assignment.sumOfNumbers = sumOfNumbers;
+  assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -41,18 +33,17 @@ console.log(sumOfNumbers(number))
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function addEvenNumbers(array) {
-    let sum =0
+function countEvenNumbers(arrayOfNumbers) {
+    let evenNumbers =0
   
-  for (let index = 0; index < array.length; index++) {
-    if(array[index] % 2===0)
-      sum += array[index]
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+    if(arrayOfNumbers[index] % 2===0)
+      evenNumbers++
   }
-  return sum
+  return evenNumbers
 }
-const numbers = [1,2,3,4,5,6,7]
-console.log(addEvenNumbers(numbers))
-// assignment.countEvenNumbers = countEvenNumbers;
+
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -68,21 +59,10 @@ console.log(addEvenNumbers(numbers))
  * @param {Array} arrayOfNumbers the array containing temperatures in Celsius to be converted
  * @returns Array the converted temperatures in Fahrenheit
  */
-function celsiusToFahrenheit(array) {
-    let newArr =[]
-
-  for (let index = 0; index < array.length; index++) {
-    let newVal= parseInt ((array[index] * 9/5) + 32)
-    newArr.push (newVal)
-  }
-  
-  // return arrayOfNumbers;
-  console.log (newArr)
-  
-  
-}
-const numberss = [120,110,70,30,50,60,78]
-  console.log(celsiusToFahrenheit(numberss))
+ function celsiusToFahrenheit(array) {
+    
+   return arrayOfNumbers;
+ }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
